ce77b417184dc0c498aea8fff6ec30e28642550f
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2021-06-30 10:38:44 +0000

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB, {
+mongoose.connect("mongodb+srv://voidrohit:Rks&18158920@cluster0.oiqtt.mongodb.net/EESAFolio?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useCreateIndex: true,
     useFindAndModify: false,
